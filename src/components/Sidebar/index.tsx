154cf08997e68e9cf7b57977367b0e64db31bc7a
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2025-06-03 20:29:53 -0300

    feat: adiciona sugestão de livros

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,28 +1,32 @@
-import { Bookmark, Home, MenuBook } from "@mui/icons-material";
-import { Box, IconButton } from "@mui/material";
+import { Bookmark, Home, MenuBook } from '@mui/icons-material';
+import { Box, IconButton } from '@mui/material';
+import { useState } from 'react';
+import Suggestions from '../Forms/suggestions';
 
 const Sidebar = () => {
+  const [MenuBookOpen, setMenuBookOpen] = useState(false);
   return (
     // <Grid item xs={1.5}>
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         py: 4,
         gap: 2,
-        position: "fixed",
+        position: 'fixed',
         left: 15,
         mr: 6,
         pr: 6,
       }}
     >
-      <IconButton sx={{ bgcolor: "#F5A623", color: "white" }}>
+      <IconButton sx={{ bgcolor: '#F5A623', color: 'white' }}>
         <Home />
       </IconButton>
-      <IconButton>
+      <IconButton onClick={() => setMenuBookOpen(true)}>
         <MenuBook />
       </IconButton>
+      <Suggestions open={MenuBookOpen} onClose={() => setMenuBookOpen(false)} />
       <IconButton>
         <Bookmark />
       </IconButton>
