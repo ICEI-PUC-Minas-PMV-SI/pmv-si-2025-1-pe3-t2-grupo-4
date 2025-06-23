f32653513e3469d9b2f0dfd46db404d2fc56f108
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2025-06-22 21:32:06 -0300

    Add missing CORS import in index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const bookRoutes = require('./routes/bookRoutes');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
