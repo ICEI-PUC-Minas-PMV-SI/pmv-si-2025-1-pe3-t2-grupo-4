3a0c90dd4f08176325a95863d20b67f645651557
João Lourenço <joaodslourenco@gmail.com>
2025-06-27 17:54:12 -0300

    fix allowed origin cors

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const bookRoutes = require('./routes/bookRoutes');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const userRoutes = require("./routes/userRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
 const allowedOrigins = [
-  'https://leitura-livre.netlify.app/',
-  'http://localhost:3000', // útil em dev local
+  "https://leitura-livre.netlify.app",
+  "http://localhost:3000", // útil em dev local
 ];
 
 app.use(
@@ -18,27 +18,27 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     },
     credentials: true, // se estiver usando cookies
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  })
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  }),
 );
 
 app.use((req, res, next) => {
-  const apiKey = req.headers['x-access-key'];
+  const apiKey = req.headers["x-access-key"];
 
   if (!apiKey || apiKey !== process.env.API_KEY) {
     return res
       .status(403)
-      .json({ error: 'Forbidden: Invalid or missing API key' });
+      .json({ error: "Forbidden: Invalid or missing API key" });
   }
   next();
 });
 
-app.use('/user', userRoutes);
-app.use('/book', bookRoutes);
+app.use("/user", userRoutes);
+app.use("/book", bookRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
